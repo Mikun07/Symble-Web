92768715c3dbc2a1253833cc91192592b50abcbc
Mikun07 <mikunolaleye@gmail.com>
2022-10-02 22:17:18 +0100

    Spacing fix

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,9 +1,23 @@
 import React from 'react'
 import './Card.css';
 
-function Card({ Number, Title, Subtxt, Pic }) {
+function Card({ Number, Title, Subtxt, Pic, Switch }) {
   return (
     <>
+      {Switch ?
+        <div className="card-container">
+          <div className="card-img">
+            <img src={Pic} alt="" />
+          </div>
+          <div className="card-txt">
+            <h3 className='card-number'>
+              {Number}
+            </h3>
+            <h6 className='card-title'>{Title}</h6>
+            <p className='card-subtxt'>{Subtxt}</p>
+          </div>
+        </div>
+        :
         <div className="card-container">
           <div className="card-txt">
             <h3 className='card-number'>
@@ -16,6 +30,7 @@ function Card({ Number, Title, Subtxt, Pic }) {
             <img src={Pic} alt="" />
           </div>
         </div>
+      }
     </>
   )
 }
--- src/Components/Screens/First-Screen/Firstscreen.js
+++ src/Components/Screens/First-Screen/Firstscreen.js
@@ -5,7 +5,6 @@ import Title from '../../Title/Title';
 import FSD1 from '../../../Assets/SVG/fsd1.svg';
 import FSD2 from '../../../Assets/SVG/fsd2.svg';
 import FSD3 from '../../../Assets/SVG/fsd3.svg';
-import Rscard from '../../Card/Rscard';
 
 function Firstscreen() {
     return (
@@ -13,7 +12,7 @@ function Firstscreen() {
             <div className="firstscreen-container">
                 <Title title="What we’re offering" />
                 <Card Number="1." Title="Share your live event and receive money in real time!" Subtxt="This means you can share your party or any free event and get monetary gifts right there on the Symble app!" Pic={FSD1} />
-                <Rscard Number="2." Title="Buy and sell products in real time!" Subtxt="Through live commerce, vendors can showcase their products more and get them sold to buyers in real time!" Pic={FSD2} />
+                <Card Switch={true} Number="2." Title="Buy and sell products in real time!" Subtxt="Through live commerce, vendors can showcase their products more and get them sold to buyers in real time!" Pic={FSD2} />
                 <Card Number="3." Title="Get paid for your live content through subscriptions!" Subtxt="Create paid events and earn from it by getting subscribers through the app!" Pic={FSD3} />
             </div>
         </>
